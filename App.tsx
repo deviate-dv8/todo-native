8b64d30dbd663f41ee75d88e1deb62eae6cc7093
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-08-06 04:14:38 +0800

    created home page

--- App.tsx
+++ App.tsx
@@ -5,23 +5,50 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-
+import {
+  Image,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import Btn from './src/components/Btn';
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <View style={styles.container}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require('./src/assets/images/cropped_logo.png')} />
+        <Text style={styles.logoText}>askmone</Text>
+      </View>
+      <Text>Your ultimate solution for a productivity app</Text>
+      <Btn style={{margin:10}} title="sign in" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    height: 100,
+    width: 100,
+    objectFit: 'contain',
+  },
+  logoContainer: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+  },
+  logoText: {
+    fontSize: 48,
+    fontWeight: 'bold',
+    left: -10,
   },
 });
 
---
+++ src/components/Btn.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { StyleSheet, Text, TouchableHighlight, ViewStyle } from "react-native";
+
+export default function Btn (props:{
+	style?:ViewStyle,
+	onPress?:Function,
+	title?:string,
+	textStyle?:Object,
+}) {
+	const [pressed, setPressed] = useState(false);
+	return (
+	<TouchableHighlight
+			onPress={() => {
+				if (props.onPress) {
+					props.onPress();
+				}
+			}}
+			onShowUnderlay={() => setPressed(true)}
+			onHideUnderlay={() => setPressed(false)}
+			style={{...styles.container,...props.style}}>
+			<Text style={[styles.text,props.textStyle,pressed && styles.pressed]}>
+				{props.title || "Click Me"}
+			</Text>
+	</TouchableHighlight>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		padding:10,
+		borderRadius: 10,
+		backgroundColor:"white",
+		borderWidth: 1,
+	},
+	text: {
+		fontSize: 16,
+		color: "black",
+	},
+	pressed: {
+		color:"white"
+	},
+})
